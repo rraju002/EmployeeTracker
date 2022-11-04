1f24bed19e057c453740f204ce7e4a87f3087d71
Rhowen01 <rraju002@ucr.edu>
2022-11-03 19:55:21 -0700

    add employee to code

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const db = mysql.createConnection({
     host: 'localhost',
     user: 'root', 
     password: '',
+    databse: 'nodemysql'
 })
 
 //Connect to MYSQL
@@ -28,3 +29,44 @@ app.get('./createdb', (req, res) => {
         res.send("Databse Created");
     });
 });
+//Create Table
+app.get('./createemployee', (req, res) => {
+    let sql = 'CREATE TABLE employee(id int AUTO_INCREMENT, name VARCHAR(255), PRIMARY KEY(id)'
+    db.query(sql ,err => {
+        if(err) {
+            throw err;
+        }
+        res.send("Database Created")
+    });
+});
+
+//Insert employee
+app.get('/employee1', (req, res) => {
+    let post = {
+        name: 'Rhowen Raju',
+        designation: 'CEO'
+    }
+    let sql = 'INSERT INTO employee SET ?'
+    let query = db.query(sql, post, err => {
+        if(err) {
+            throw err
+        }
+        res.send('Employee Added')
+    })
+})
+
+//Select Employee
+app.get('./getemployee', (req, res) => {
+    let sql = 'SELECT * FROM employee'
+    let query = db.query(sql, (err, results) => {
+        if(err) {
+            throw err
+        }
+        console.log(results)
+        res.send('Employee details received')
+    })
+} )
+
+app.listen('3000', () => {
+    console.log('Server Started on Port 3000')
+})
