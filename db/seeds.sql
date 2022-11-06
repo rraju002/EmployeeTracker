dbf101796fe4f635fa455ca416d3a78b01cc57a5
Rhowen01 <rraju002@ucr.edu>
2022-11-06 10:37:25 -0800

    add dept, roles and employees

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO department
+(name)
+
+--add departments, roles and employees 
+
+VALUES 
+("HR")
+("IT")
+("Buisness")
+("Marketing")
+
+INSERT INTO roles
+(title, salary, department)
+
