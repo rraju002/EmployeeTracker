f89beca33b51a5a96608205f1c085ff831d1ea3e
Rhowen01 <rraju002@ucr.edu>
2022-11-06 10:51:36 -0800

    finish adding employee func and adjust some syntax

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,22 +4,22 @@ INSERT INTO department
 --add departments, roles and employees 
 
 VALUES 
-("HR")
-("IT")
-("Buisness")
-("Marketing")
+("HR");
+("IT");
+("Buisness");
+("Marketing");
 
 --create pos and salary 
 INSERT INTO roles
-(title, salary, department)
-("CEO", 100000)
-("COO", 90000)
-("Secretary", 60000)
-("Intern", 20000)
+(title, salary, department),
+("CEO", 100000, 1),
+("COO", 90000, 2),
+("Secretary", 60000, 3),
+("Intern", 20000, 4),
 
 INSERT INTO employees
 (first_name, last_name, role_id)
-("Naruto Uzumaki")
-("Itachi Uchiha")
-("Sauske Uchiha")
-("Kiba")
+("Naruto Uzumaki", 1),
+("Itachi Uchiha", 2),
+("Sauske Uchiha", 3),
+("Kiba", 4); 
