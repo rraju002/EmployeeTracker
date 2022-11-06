1a1672c6eeccb3118b462a39f612a41e82c8b060
Rhowen01 <rraju002@ucr.edu>
2022-11-06 10:45:52 -0800

    add pos and salary

--- db/seeds.sql
+++ db/seeds.sql
@@ -9,6 +9,17 @@ VALUES
 ("Buisness")
 ("Marketing")
 
+--create pos and salary 
 INSERT INTO roles
 (title, salary, department)
+("CEO", 100000)
+("COO", 90000)
+("Secretary", 60000)
+("Intern", 20000)
 
+INSERT INTO employees
+(first_name, last_name, role_id)
+("Naruto Uzumaki")
+("Itachi Uchiha")
+("Sauske Uchiha")
+("Kiba")
