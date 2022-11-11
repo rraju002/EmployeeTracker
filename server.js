18a54505e23e7a8116468a8212347591fe847c0c
Rhowen01 <rraju002@ucr.edu>
2022-11-11 11:33:03 -0800

    add server.js

---
+++ server.js
@@ -0,0 +1,43 @@
+const inquirer = require("inquirer");
+const sqlDatabase = require("./index")
+const db = require("./index")
+
+//Connect to mysql
+const connection = mysql.createConnection({
+    host: 'localhost',
+    //Port Connection
+    port: 3306,
+
+    //Username
+    user: 'root',
+
+    //Password
+    password: 'Team7',
+    database: ''
+})
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    console.log(`
+    ╔═══╗─────╔╗──────────────╔═╗╔═╗
+    ║╔══╝─────║║──────────────║║╚╝║║
+    ║╚══╦╗╔╦══╣║╔══╦╗─╔╦══╦══╗║╔╗╔╗╠══╦═╗╔══╦══╦══╦═╗
+    ║╔══╣╚╝║╔╗║║║╔╗║║─║║║═╣║═╣║║║║║║╔╗║╔╗╣╔╗║╔╗║║═╣╔╝
+    ║╚══╣║║║╚╝║╚╣╚╝║╚═╝║║═╣║═╣║║║║║║╔╗║║║║╔╗║╚╝║║═╣║
+    ╚═══╩╩╩╣╔═╩═╩══╩═╗╔╩══╩══╝╚╝╚╝╚╩╝╚╩╝╚╩╝╚╩═╗╠══╩╝
+    ───────║║──────╔═╝║─────────────────────╔═╝║
+    ───────╚╝──────╚══╝─────────────────────╚══╝`)
+    // runs the app
+    firstPrompt();
+});
+
+//Main Menu
+const menu = [
+    {
+        type: "list",
+        name: "first_name",
+        message: "What is the employee's first name?"
+    }
+
+]
